5914e51e0352edf3957a51953f054912482ae8a1
yuyan07 <86942523+yuyan07@users.noreply.github.com>
2021-07-09 11:58:55 +0700

    Generasi GIGIH Module 1 Session 2 homework

---
+++ Module-1/Session 2/script.js
@@ -0,0 +1,25 @@
+let btnSubmit = document.getElementById("tombol");
+
+// do something if the button click
+btnSubmit.addEventListener("click", () => {
+  alert("Hello World!");
+});
+
+
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		//Typical action to be performed when the document is ready:
+		var data = JSON.parse(xhttp.responseText);
+		console.log(data);
+	} else {
+		alert(`error = ${xhttp.responseText}`);
+    // console.log(`error : ${xhttp.responseText}`);
+	}
+};
+
+xhttp.open("GET",
+ "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json",
+ true);
+xhttp.send();
+ 
